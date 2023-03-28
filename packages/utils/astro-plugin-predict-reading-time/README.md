f93d3974ddb8b6de84e13ffb34c16f0c91c0d398
Sergey Lukin <contact@sergeylukin.com>
2023-03-28 10:31:45 +0300

    implement @depla/utils-astro-plugin-predict-reading-time

---
+++ packages/utils/astro-plugin-predict-reading-time/README.md
@@ -0,0 +1,7 @@
+# utils-astro-plugin-predict-reading-time
+
+This library was generated with [Nx](https://nx.dev).
+
+## Building
+
+Run `nx build utils-astro-plugin-predict-reading-time` to build the library.
---
+++ packages/utils/astro-plugin-predict-reading-time/src/lib/utils-astro-remark-plugin-predict-reading-time.ts
@@ -0,0 +1,11 @@
+import getReadingTime from 'reading-time';
+import { toString } from 'mdast-util-to-string';
+
+export function readingTimeRemarkPlugin() {
+  return function (tree: any, file: any) {
+    const textOnPage = toString(tree);
+    const readingTime = Math.ceil(getReadingTime(textOnPage).minutes);
+
+    file.data.astro.frontmatter.readingTime = readingTime;
+  };
+}
