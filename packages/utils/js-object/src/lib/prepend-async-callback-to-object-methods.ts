35baa141b88ec7fa006955cf3ce08aca4dc6dd20
Sergey Lukin <contact@sergeylukin.com>
2023-04-26 08:59:23 +0300

    feat(js-object): let configure before() after() hooks

--- packages/utils/js-object/src/lib/prepend-async-callback-to-object-methods.ts
+++ packages/utils/js-object/src/lib/prepend-async-callback-to-object-methods.ts
@@ -17,12 +17,14 @@ export const prependAsyncCallbackToObjectMethods = (Foo, cb) => {
     // before and after method execution.
     Foo[name] = async (...args) => {
       // all arguments that the method receives are in the 'arguments' object
-      await cb.call(this, name);
+      const { before, after } = cb.call(this, name);
 
       // now we call the original method, _method1, on this with all arguments we received
       // this is probably the most confusing line of code here ;)
       // (I never user this['method'] before - but it works)
+      await before();
       const result = await Foo['_' + name].apply(Foo, args);
+      await after();
 
       // here is the post-execution logging
 
