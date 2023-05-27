912eca067f1af31b0f9516903e94e941b7ddc4f8
Sergey Lukin <contact@sergeylukin.com>
2023-05-27 16:46:52 +0300

    fix(depla-slice-nx-workspace): overwrite nx.json

--- packages/depla-slice-nx-workspace/src/lib/generate.ts
+++ packages/depla-slice-nx-workspace/src/lib/generate.ts
@@ -8,17 +8,15 @@ export const generate = (workspace: any) => {
   console.log('RUNNING GENERATOR WITH FOLLOWING WORKPSACE', workspace);
   return {
     runBefore: [
-      [
-        `${VOLTA_BINARY} run --node ${NODE_VERSION} \
+      `${VOLTA_BINARY} run --node ${NODE_VERSION} \
         npx --yes create-nx-workspace@15.7.2 ${workspace.name} \
             --preset=ts \
             --nxCloud=false --pm=npm`,
-        // although this file doesn't have `generated` in it's path
-        // (our internal convention to separate files that can be overwritten
-        // from others)
-        // we want to overwrite it every time so let's delete it first
-        `rm -f nx.json`,
-      ],
+      // although this file doesn't have `generated` in it's path
+      // (our internal convention to separate files that can be overwritten
+      // from others)
+      // we want to overwrite it every time so let's delete it first
+      [`rm -f nx.json`, resolve(workspace.name)],
       // [
       //   `sed -i '' '2s/^/"plugins": ["@sergeylukin\\/nxtensions-astro"],\n/' nx.json`,
       //   resolve(workspace.name),
