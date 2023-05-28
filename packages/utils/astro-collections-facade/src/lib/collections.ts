d7636a0d0814148d0a4b51e4f913bf5ec8ed8930
Sergey Lukin <contact@sergeylukin.com>
2023-05-28 20:52:13 +0300

    fix(depla-utils-astro-collections-facade): export as ESM

--- packages/utils/astro-collections-facade/src/lib/collections.ts
+++ packages/utils/astro-collections-facade/src/lib/collections.ts
@@ -1,10 +1,5 @@
 // @ts-ignore
-let content;
-try {
-  content = require('astro:content');
-} catch (e) {
-  console.log('failed to load astro:content');
-}
+import { getCollection, getEntryBySlug } from 'astro:content';
 
-export const getCollection = content.getCollection;
-export const getEntryBySlug = content.getEntryBySlug;
+export { getCollection };
+export { getEntryBySlug };
