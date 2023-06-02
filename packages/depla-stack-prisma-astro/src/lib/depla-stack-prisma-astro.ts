4f8b4d957a5bb50520f3493fe56a2939f0519bf0
Sergey Lukin <contact@sergeylukin.com>
2023-06-02 05:46:34 +0300

    feat(depla-slice-astro-app): git commit when finished

--- packages/depla-stack-prisma-astro/src/lib/depla-stack-prisma-astro.ts
+++ packages/depla-stack-prisma-astro/src/lib/depla-stack-prisma-astro.ts
@@ -56,6 +56,8 @@ export const generate = ({
         ? [
             `DATABASE_URL='file:./dev.db' nx setup-prisma ${app.name}`,
             `npx prisma migrate dev --name init`,
+            `git add`,
+            `git commit --author="Dep La <la@depl.la>" -m "infra: depla initial commit"`,
           ]
         : []),
     ],
