6a4f69a8167408690aae8ac2bd5365dda69182dd
Sergey Lukin <contact@sergeylukin.com>
2023-09-18 17:38:10 +0300

    chore: get rid of clutter in homepage

--- apps/website/src/pages/api/niches.js
+++ apps/website/src/pages/api/niches.js
@@ -3,7 +3,7 @@ import { POCKET } from '../../services/PocketBase.ts';
 const getApps = async () => {
   POCKET.admins.authWithPassword(
     'contact@sergeylukin.com',
-    'axc*pkb-HNP2gru2nwe'
+    'axc*pkb-HNP2gru2nwe' // @TODO you didn't see this, will replace with ENV soon
   );
   const records = await POCKET.collection('categories').getFullList({
     sort: '-created',
