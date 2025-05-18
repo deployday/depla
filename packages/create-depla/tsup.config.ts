a479b9ec026fd34ea694e4dce1a5bd0db77fe13f
Sergey Lukin <contact@sergeylukin.com>
2025-05-19 01:14:03 +0300

    cleanup

---
+++ packages/create-depla/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  format: ["esm"], // 👈 Ensure ESM format
+  target: "node20",
+  outDir: "dist",
+  splitting: false,
+  clean: true,
+  shims: false,
+});
