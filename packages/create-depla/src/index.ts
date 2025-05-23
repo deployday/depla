dee0901fee9b23a64c59d039aac0d63fd8c263a2
Sergey Lukin <contact@sergeylukin.com>
2025-05-23 12:27:10 +0300

    fix dependencies

--- packages/create-depla/src/index.ts
+++ packages/create-depla/src/index.ts
@@ -1,4 +1,21 @@
-#!/usr/bin/env node
 import { scaffoldProject } from "@depla/depla";
+import { Command } from "commander";
 
-scaffoldProject();
+const program = new Command();
+
+program
+  .name("create-depla")
+  .option("-t, --template <template>", "template to use (e.g. basic or user/repo)")
+  .option("-n, --name <name>", "project name")
+  .parse(process.argv);
+
+const opts = program.opts();
+
+await scaffoldProject({
+  template: opts.template || "basic",
+  targetDir: opts.name || "my-depla-app",
+  variables: {
+    projectName: opts.name || "my-depla-app",
+    author: process.env.USER || "anonymous",
+  },
+});
